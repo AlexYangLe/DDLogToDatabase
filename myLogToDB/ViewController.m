ff605b8382ed2e79b6089aa7cc9f6d7b400c7e3b
alexYang <alexyang@gumptech.com>
2016-04-14 11:23:11 +0800

    Initial Commit

---
+++ myLogToDB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  myLogToDB
+//
+//  Created by 杨乐乐 on 16/4/14.
+//  Copyright © 2016年 杨乐乐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
